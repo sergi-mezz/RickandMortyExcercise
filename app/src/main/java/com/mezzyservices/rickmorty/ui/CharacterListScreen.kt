3bf4f50e51b60e60123ead3d01d9428bbd01907b
sergi-mezz <sergio_isc@yahoo.com>
2025-10-05 21:43:11 -0600

    Se agregan filtros de especie y estatus

--- app/src/main/java/com/mezzyservices/rickmorty/ui/CharacterListScreen.kt
+++ app/src/main/java/com/mezzyservices/rickmorty/ui/CharacterListScreen.kt
@@ -15,8 +15,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.sharp.Favorite
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,6 +30,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -44,7 +47,6 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import coil3.compose.AsyncImage
 import com.mezzyservices.rickmorty.data.model.Character
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
@@ -54,67 +56,91 @@ fun CharacterListScreen(
     onFavouritesClicked: () -> Unit
 ) {
     val viewModel = hiltViewModel<CharacterListViewModel>()
+    val characters = viewModel.characterListFlow.collectAsLazyPagingItems()
+    val scope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+    var query by rememberSaveable { mutableStateOf("") }
+
+    var statusFilterList by remember { mutableStateOf(setOf("Ver todos")) }
+    var specieFilterList by remember { mutableStateOf(setOf("Ver todas")) }
+    var statusFilterSelection by remember { mutableStateOf("") }
+    var specieFilterSelection by remember { mutableStateOf("") }
+
     val listState = rememberSaveable(saver = LazyListState.Saver) {
         LazyListState()
     }
-    val characters = viewModel.characterListFlow.collectAsLazyPagingItems(Dispatchers.IO)
-    var query by rememberSaveable { mutableStateOf("") }
 
     PullToRefreshBox(
-        modifier = Modifier.fillMaxSize(),
-        onRefresh = { characters.refresh() },
-        isRefreshing = characters.loadState.refresh is LoadState.Loading
+        isRefreshing = characters.loadState.refresh is LoadState.Loading,
+        onRefresh = { characters.refresh() }
     ) {
-
-        val scope = rememberCoroutineScope()
-        val snackBarHostState = remember { SnackbarHostState() }
-
         Scaffold(
-            topBar = { TopAppBar(modifier = Modifier.padding(top = 25.dp), query, onFavouritesClicked, { query = it } ) },
+            topBar = {
+                TopAppBar(
+                    modifier = Modifier.padding(top = 5.dp),
+                    query,
+                    onFavouritesClicked,
+                    { query = it },
+                    statusFilterList,
+                    specieFilterList,
+                    { specieFilterSelection = if (it == "Ver todas") "" else it },
+                    { statusFilterSelection = if (it == "Ver todos") "" else it }
+                )
+            },
             snackbarHost = { SnackbarHost(hostState = snackBarHostState) }
         ) { innerPadding ->
 
             when (characters.loadState.refresh) {
-                is LoadState.Loading -> {
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        CircularProgressIndicator()
-                    }
-                }
+                is LoadState.Loading -> {}
 
                 is LoadState.Error -> {
-                    scope.launch {
-                        snackBarHostState.showSnackbar(characters.loadState.refresh.toString())
+                    LazyColumn(modifier = Modifier.fillMaxSize()) {}
+                    LaunchedEffect("refresh_error") {
+                        scope.launch {
+                            snackBarHostState.showSnackbar(
+                                (characters.loadState.refresh as LoadState.Error).error.message
+                                    ?: "Error"
+                            )
+                        }
                     }
                 }
 
-                else -> {
-                    if(query.isEmpty()) {
-                        LazyColumn(
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            state = listState
-                        ) {
+                is LoadState.NotLoading -> {
+
+                    val tempStatusSet = mutableSetOf<String>()
+                    val tempSpecieSet = mutableSetOf<String>()
+                    characters.itemSnapshotList.items.forEach {
+                        it.status?.let { tempStatusSet.add(it) }
+                        it.species?.let { tempSpecieSet.add(it) }
+                    }
+
+                    statusFilterList = statusFilterList + tempStatusSet
+                    specieFilterList = specieFilterList + tempSpecieSet
+
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        state = listState
+                    ) {
+                        if (query.isEmpty() && statusFilterSelection.isEmpty() && specieFilterSelection.isEmpty()) {
                             items(characters.itemCount, characters.itemKey { it.id!! }) { index ->
-                                EpisodeCard(characters[index]!!, onCardClicked = {
-                                    onCardClicked(characters[index]!!.id!!)
-                                })
+                                if (characters[index] != null)
+                                    CharacterCard(characters[index]!!, onCardClicked = {
+                                        onCardClicked(characters[index]!!.id!!)
+                                    })
                             }
-                        }
-                    } else {
-                        LazyColumn(
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            items(characters.itemSnapshotList.items.filter { it.name!!.contains(query, true) }) { it ->
-                                EpisodeCard(it, onCardClicked = {
+                        } else
+                            items(characters.itemSnapshotList.items.filter {
+                                it.name!!.contains(query, true) && it.species!!.contains(
+                                    specieFilterSelection
+                                ) && it.status!!.contains(statusFilterSelection)
+                            }) { it ->
+                                CharacterCard(it, onCardClicked = {
                                     onCardClicked(it.id!!)
                                 })
                             }
-                        }
                     }
                 }
             }
@@ -128,7 +154,11 @@ fun TopAppBar(
     modifier: Modifier,
     query: String,
     onFavouritesClicked: () -> Unit,
-    onQueryChanged: (String) -> Unit
+    onQueryChanged: (String) -> Unit,
+    statusFilterList: Set<String>,
+    specieFilterList: Set<String>,
+    onSpeciesSelected: (String) -> Unit,
+    onStatusSelected: (String) -> Unit
 ) {
 
     Column(modifier = modifier) {
@@ -142,8 +172,13 @@ fun TopAppBar(
                     },
                     expanded = false,
                     onExpandedChange = { },
-                    placeholder = {Text("Busqueda")},
-                    trailingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "")}
+                    placeholder = { Text("Busqueda") },
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = ""
+                        )
+                    }
                 )
             },
             expanded = false,
@@ -152,8 +187,60 @@ fun TopAppBar(
 
         }
 
-        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
-            Text("TODO Filtros")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Text("Filtrar por:")
+                Box() {
+                    var expanded by remember { mutableStateOf(false) }
+                    Button(
+                        onClick = { expanded = !expanded }
+                    ) {
+                        Text("Estatus")
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            statusFilterList.forEach {
+                                DropdownMenuItem(
+                                    text = { Text(it) },
+                                    onClick = {
+                                        expanded = false
+                                        onStatusSelected(it)
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+                Box() {
+                    var expanded by remember { mutableStateOf(false) }
+                    Button(
+                        onClick = { expanded = !expanded }
+                    ) {
+                        Text("Especie")
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+
+                            specieFilterList.forEach {
+                                DropdownMenuItem(
+                                    text = { Text(it) },
+                                    onClick = {
+                                        expanded = false
+                                        onSpeciesSelected(it)
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+
+            }
             IconButton(onClick = {
                 onFavouritesClicked()
             }) {
@@ -165,10 +252,12 @@ fun TopAppBar(
 }
 
 @Composable
-fun EpisodeCard(character: Character, onCardClicked: () -> Unit) {
-    Card(modifier = Modifier
-        .padding(5.dp)
-        .clickable(onClick = { onCardClicked() })) {
+fun CharacterCard(character: Character, onCardClicked: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(5.dp)
+            .clickable(onClick = { onCardClicked() })
+    ) {
         Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
             AsyncImage(
                 modifier = Modifier.size(100.dp),
